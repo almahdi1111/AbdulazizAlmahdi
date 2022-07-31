34062c4c763953a65fce9b81304925b9e0191d01
abdulaziz <almahdi@gmail.com>
2022-07-31 09:59:06 +0300

    Add all Entity Models

---
+++ Domain/Entities/Application User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Application_User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "ادخل الاسم ")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "ادخل رقم الهاتف"), MaxLength(9, ErrorMessage = "يجب ان لا يكون رقم الهاتف اكثر من تسعة ارقام "), MinLength(9, ErrorMessage = "يجب ان لا يكون رقم الهاتف اقل من تسعة ارقام")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "ادخل اسم المسنخدم")]
+        [Display(Name = "User Name")]
+        // [Remote(action: "IsUserNameExist", controller:"Users",ErrorMessage ="اسم المستخدم موجود مسبقا")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "يرجى ادخال كلمة المرور")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "يرجى اعادة ادخال كلمة المرور"), Compare("Password", ErrorMessage = "كلمة المرور غير متطابقة")]
+        public string PasswordConfirm { get; set; }
+        public int Type { get; set; }
+       /* public virtual ICollection<Support_Messages> messages { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+*/
+    }
+}
---
+++ Domain/Entities/CartItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class CartItem
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public double TotalPrice
+        {
+            get
+            {
+                return Price * Quantity;
+            }
+        }
+
+
+    }
+}
